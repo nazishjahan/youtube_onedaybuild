d4ef59d6995cbdbbb7a6f26d16a2303e7caf7901
nazish jahan <nazishjahan04@yahoo.com>
2020-07-07 10:35:00 +0500

    initial xcode project creations

---
+++ youtube_onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube_onedaybuild
+//
+//  Created by NAZISH ZOHAIB on 07/07/2020.
+//  Copyright © 2020 NAZISH ZOHAIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
